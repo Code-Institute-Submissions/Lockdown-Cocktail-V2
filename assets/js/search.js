311d1540be25d3e24ef47021484d9d6ef8dc8767
Sarah Telesia <66113301+Telesia@users.noreply.github.com>
2020-12-03 13:35:34 +0000

    Adjust function to call from API

--- assets/js/search.js
+++ assets/js/search.js
@@ -5,17 +5,51 @@
 const form = document.getElementById("cocktail-form");
 const userSearch = document.getElementById("cocktail-form-searchbar");
 
-
+userSearch.addEventListener("keyup", e => { 
+    const searchString = e.target.value; 
+  });
+console.log()
 
 //Cocktaildb API data call
-function submitted(event) {
+/*function submitted(event) {
     event.preventDefault();
-    const response = (`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userSearch.value}`);
+    var response = (`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userSearch.value}`);
     console.log(response); //this works yey!
-    let jsonResponse = response.responseType = 'json';
-    //var jsonResponse = JSON.parse(response); // this bit doesn't boo!
+    
+    var jsonResponse = JSON.parse(response); // this bit doesn't boo!
     
     console.log(jsonResponse);    
+}*/ 
+
+
+//I updated the submitted function to the following:
+function submitted(event) {
+    event.preventDefault();
+    let x = userSearch.value;
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${x}`;
+    fetch(url)
+        .then(response => {
+        return response.json();
+    })
+    .then(fetchedData => {
+        apiData = fetchedData;
+    })
+    .catch(error => {
+        alert("Failed to get api data.");
+        console.log(error);
+    });
+    // console.log(response); //this works yey!
+    // let jsonResponse = response.responseType = 'json';
+    //var jsonResponse = JSON.parse(response); // this bit doesn't boo!
+    
+    setTimeout(() => {
+        console.log(apiData);
+    }, 1000);
+    // console.log(jsonResponse);    
 }
 
 form.addEventListener("submit", submitted);
+
+
+
+
