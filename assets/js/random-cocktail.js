0feace2a0a125be7c20e23c81d0144ec28de83d2
Sarah Telesia <66113301+Telesia@users.noreply.github.com>
2020-12-08 21:00:17 +0000

    Update function to call a random cocktail to include .catch error method

--- assets/js/random-cocktail.js
+++ assets/js/random-cocktail.js
@@ -1,18 +1,17 @@
-// Set variables*/
+// Set variables
 
 let randomButton = document.getElementById("random-btn");
 let displayRandomCocktail = document.getElementById("image");
 
 
 
-// Random Cocktail Button Click Event*/
+// Random Cocktail Button Click Event
 
-//randomButton.addEventListener('click', randomCocktail);--> /*target.addEventListener(event(click,load etc.), function, useCapture)*/
-
+//randomButton.addEventListener('click', randomCocktail);--> /*target.addEventListener(event(click,load etc.), function, useCapture)
 
 //Function to generate random cocktail
 
-async function randomCocktail() {
+/*async function randomCocktail() {
     const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php/preview");
     const jsonresponse = await response.json();
     console.log(jsonresponse);
@@ -25,7 +24,7 @@ async function randomCocktail() {
     console.log(typeof(data));
     console.log(typeof(img));
     /*document.getElementById("cocktail-card").innerHTML = drinkName;
-    document.getElementById("data").innerHTML = data;*/
+    document.getElementById("data").innerHTML = data;
     displayRandomCocktail.innerHTML = `
     <div id="random-cocktail-card" class="random-cocktail-card" style="width:fluid">
     <div class="card-body text-center">
@@ -35,6 +34,40 @@ async function randomCocktail() {
     </div>
     </div>`; //credit: https://www.codegrepper.com/code-examples/javascript/how+to+display+image+in+html+from+json+object
     
+}*/
+
+function randomCocktail() {
+    const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php/preview";
+    fetch(url)
+        .then(response => {
+        return response.json(); //return response as json object
+    })
+    .then(fetchedData => {
+        apiData = fetchedData;
+        let drinkName = apiData.drinks[0].strDrink;
+        let data = apiData.drinks[0].strInstructions;
+        let img = apiData.drinks[0].strDrinkThumb;
+        displayRandomCocktail.innerHTML = `
+        <div id="search-cocktail-card" class="card" style="width:fluid">
+        <div class="card-body text-center">
+        <h5 class="card-title">${drinkName}</h5>
+        <p class="card-text">${data}</p>
+        <img class="random-cocktail-image img-fluid" src="${img}">
+        </div>
+        </div>`; 
+        
+         // variable apiData = returned json data
+         // Unpack data into variables for specific number in the array
+    })
+    .catch(error => {
+        alert("Failed to get api data.");
+        console.log(error);
+    });
+    
+    setTimeout(() => {
+        console.log(apiData);
+    }, 1000);
+    // console.log(jsonResponse);    
 }
 
 //how to add this into code in a nice way to catch errors?
